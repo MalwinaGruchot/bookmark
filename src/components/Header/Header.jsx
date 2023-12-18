cf993f353a4e742e202afd12f87e85a2b01ab392
MalwinaGruchot <malwinagruchot@wp.pl>
2023-12-18 19:59:24 +0100

    make header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import logo from "../..//assets/logo-bookmark.svg";
+import HeaderMenu from "../HeaderMenu/HeaderMenu";
+import styles from "./Header.module.scss";
+
 export default function Header() {
   return (
-    <header>
-      <div></div>
+    <header className={styles.container}>
+      <img src={logo} />
+      <HeaderMenu></HeaderMenu>
     </header>
   );
 }
