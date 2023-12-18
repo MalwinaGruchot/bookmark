e4174a42f99c17d06eda8c13d3a08a8a1289a9ae
MalwinaGruchot <malwinagruchot@wp.pl>
2023-12-19 00:07:41 +0100

    make first section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import "./App.scss";
 import "./Header";
 import Header from "./Header";
+import SectionBookmarkMenager from "./SectionBookmarkMenager/SectionBookmarkMenager";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <SectionBookmarkMenager></SectionBookmarkMenager>
+      </main>
     </>
   );
 }
---
+++ src/components/SectionBookmarkMenager/SectionBookmarkMenager.jsx
@@ -0,0 +1,27 @@
+import styles from "./SectionBookmarkMenager.module.scss";
+import hero from "../../assets/illustration-hero.svg";
+import Button from "../Button";
+
+export default function SectionBookmarkMenager() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.column}>
+          <h2 className={styles.title}>A Simple Bookmark Menager</h2>
+          <p className={styles.text}>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly.Try it for
+            free.
+          </p>
+          <div className={styles.wrapper}>
+            <Button color={"blue"}>Get it on Chrome</Button>
+            <Button color={"gray"}>Get it on Firefox</Button>
+          </div>
+        </div>
+        <div className={styles.circle}>
+          <img src={hero} />
+        </div>
+      </div>
+    </section>
+  );
+}
