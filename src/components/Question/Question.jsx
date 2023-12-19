e6e60eda7500be984fe01e7db7e06185390d9c01
MalwinaGruchot <malwinagruchot@wp.pl>
2023-12-20 00:00:07 +0100

    make question

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import styles from "./Question.module.scss";
-import arrow from "../../assets/icon-arrow.svg";
 
 export default function Question({ children, answer }) {
   const [active, setActive] = useState(false);
@@ -8,32 +7,33 @@ export default function Question({ children, answer }) {
     <div className={styles.wrapper}>
       <div className={styles.wrapp} onClick={() => setActive(!active)}>
         <h4 className={styles.title}>{children}</h4>
-        <img
-          className={styles.icon}
-          // onClick={() => setActive(!active)}
-          src={arrow}
-          style={active ? { stroke: "red" } : {}}
-        />
-        {/* <svg
+
+        <svg
           width="20"
           height="10"
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
-        > */}
-        {/* <polyline
-            points="0 20 20 0 40 20"
-            stroke="blue"
-            fill="none"
-            strokeWidth="4"
-          /> */}
-        {/* <polyline
-            points="0 0 10 10 20 0 "
-            stroke="blue"
-            fill="none"
-            strokeWidth="4"
-            strokeLinejoin="miter"
-          />
-        </svg> */}
+        >
+          {active ? (
+            <polyline
+              points="0,10 10,0 20,10"
+              style={{
+                stroke: "hsl(0, 94%, 66%)",
+                fill: "none",
+                strokeWidth: "2px",
+              }}
+            />
+          ) : (
+            <polyline
+              points="0,0 10,10 20,0"
+              style={{
+                stroke: "hsl(231, 69%, 60%)",
+                fill: "none",
+                strokeWidth: "2px",
+              }}
+            />
+          )}
+        </svg>
       </div>
       {active ? <p className={styles.text}>{answer}</p> : null}
     </div>
